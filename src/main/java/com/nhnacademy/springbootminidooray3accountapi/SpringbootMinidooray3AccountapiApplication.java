815e8611ea7119a2591f58c7159a90bc694569cd
rosin23 <rosinessrosin@gmail.com>
2024-03-08 14:32:52 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/springbootminidooray3accountapi/SpringbootMinidooray3AccountapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootminidooray3accountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMinidooray3AccountapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMinidooray3AccountapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/springbootminidooray3accountapi/SpringbootMinidooray3AccountapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springbootminidooray3accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMinidooray3AccountapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
